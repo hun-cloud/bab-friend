d2cd20bdfc99eeea9dbce133d7739d9524a209a3
leejaehun <ljh0901@mlsoft.com>
2023-12-04 11:26:44 +0900

    init

---
+++ consumer/src/test/java/babfriend/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package babfriend.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
