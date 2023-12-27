a8438486be220dc980a042b8189324ef4c80b074
JaeHunJaeHun <jhl9838@gmail.com>
2023-12-27 20:17:42 +0900

    cors 전체 허용

--- api/src/main/java/babfriend/api/common/config/WebMvcConfig.java
+++ api/src/main/java/babfriend/api/common/config/WebMvcConfig.java
@@ -15,7 +15,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 
     @Override
